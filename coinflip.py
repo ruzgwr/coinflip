88d50ed00c2e60c93b0951dd830f4f8452daa027
Klaener <kancamatakilankar@gmail.com>
2022-05-03 14:41:38 +0300

    Create coinflip.py

---
+++ coinflip.py
@@ -0,0 +1,28 @@
+import random
+
+def flip():
+	flip = random.random()
+	if (flip>=.5):
+		return True
+	else:
+		return False
+
+def main(num):
+	heads = 0
+	tails = 0
+	resultString = ""
+
+	for i in range(int(num)):
+		if (flip()):
+			heads+=1
+			resultString += "H"
+		else:
+			tails+=1
+			resultString += "T"
+	
+	print "Number of Heads: %i" % (heads)
+	print "Number of Tails: %i" % (tails)
+	print resultString
+
+userInput = input("Please enter a number of flips: ")
+main(userInput)
